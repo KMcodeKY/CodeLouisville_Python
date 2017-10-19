be7bf939a13b760dce1e8bb1d5a903a838c0989e
KM <you@example.com>
2017-10-19 17:18:00 -0400

    Project setup

---
+++ .virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\katemo~1\desktop\treeho~1\python\python~4\virtua~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
