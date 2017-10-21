5bc45f1b0e2be6af5dc8e756b1caacbab3c625b6
KM <you@example.com>
2017-10-21 18:00:36 -0400

    Added Category and ToDo models

--- myproject/checklist/models.py
+++ myproject/checklist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.type
+
+
+class ToDo(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('checklist:detail', kwargs={'pk': self.pk})
